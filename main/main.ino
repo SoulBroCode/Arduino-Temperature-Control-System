61bc7762bd0eb18897c89127d5088d35cee11369
SoulBroCode <linhhuynh2000@gmail.com>
2023-08-10 11:56:22 +0100

    Intial commit

---
+++ main/main.ino
@@ -0,0 +1,20 @@
+
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println();
+  Serial.print(F("------- Starting --------"));
+  Serial.println();
+}
+
+void loop() {
+   delay(3000);
+}
+
+
+
+
+
+
+
+
