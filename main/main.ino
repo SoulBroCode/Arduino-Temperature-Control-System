57ad9df3803b96bbac417c80ccd95bdbe9db95e6
SoulBroCode <linhhuynh2000@gmail.com>
2023-09-11 13:11:51 +0100

    Temperatures comparison, switching fan state

--- main/main.ino
+++ main/main.ino
@@ -1,4 +1,26 @@
+#include <DS18B20.h>
+
+#include "TemperatureSensor.h"
 #include "Button.h"
+
+const float MAX_TEMP = 80;
+const float MIN_TEMP = -20;
+float _ChangeableTemp = 30; 
+float _InsideTemp = 0;
+float _OutsideTemp = 0;
+
+uint8_t _TemperatureSensorCount = 0;
+DS18B20 _DS18B20(8);
+TemperatureSensor** _TemperatureSensors;
+
+
+//Loop State
+enum State{
+  Normal,
+  Edit
+};
+
+State _State(Normal);
 
 Button _UpBtn(2);
 Button _DownBtn(4);
@@ -17,6 +39,19 @@ void setup() {
   Serial.println();
   Serial.print(F("------- Starting --------"));
   Serial.println();
+
+  //Sensor
+   _TemperatureSensorCount = _DS18B20.getNumberOfDevices();
+  _TemperatureSensors =  new TemperatureSensor*[_TemperatureSensorCount];
+
+  Serial.print(F("Devices : ")); 
+  Serial.print(_TemperatureSensorCount);  
+  Serial.println();
+
+  for(int i = 0; i < _TemperatureSensorCount; i++)
+  {
+    _TemperatureSensors[i] = new TemperatureSensor(i, _DS18B20);
+  }
 }
 
 void loop() {
@@ -33,8 +68,51 @@ void loop() {
 }
 
 void NormalLoop(){
+  if(_TemperatureSensorCount < TemperatureSensor::Unknown)
+  {
+    Serial.println();
+    Serial.println(F("-------------------"));
+    Serial.println(F("Error : Missing Sensor"));
+
+    display.clearDisplay();
+
+    display.setTextSize(2);   
+    display.setTextColor(SSD1306_WHITE);       
+    display.setCursor(0,0); 
+
+    display.print(F("Error"));
+    display.println();
+    display.print(F("Missing"));
+    display.println();
+    display.print(F("Sensor"));
+    display.display();
+
+    delay(5000);
+
+    return;
+  } 
+
   Serial.println();
   Serial.println(F("-------------------"));
+
+  _InsideTemp = _TemperatureSensors[TemperatureSensor::Inside]->GetTempC();
+  _OutsideTemp = _TemperatureSensors[TemperatureSensor::Outside]->GetTempC();
+
+  if(_InsideTemp < _ChangeableTemp)
+  {
+    if(_OutsideTemp > _InsideTemp){
+      _NewFanState = On;
+      //Serial.print(F("Turning ON fans"));
+    }
+    else{
+      _NewFanState = Off;
+      //Serial.print(F("Turning OFF fans"));
+    }
+  }
+  else{
+    _NewFanState = Off;
+    //Serial.print(F("Turning OFF fans"));
+  }
 } 
 
 void EditLoop(){
