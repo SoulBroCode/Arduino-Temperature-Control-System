e9d5346422156789fa4bf914560de477ca727354
SoulBroCode <linhhuynh2000@gmail.com>
2023-09-13 10:43:17 +0100

    Make control temperature ajustable

--- main/main.ino
+++ main/main.ino
@@ -26,6 +26,8 @@ Button _UpBtn(2);
 Button _DownBtn(4);
 Button _EditBtn(7);
 
+//Fan Relay Condition
+const int FAN_RELAY_PIN = 11;
 enum FanState{
   On,
   Off
@@ -33,6 +35,8 @@ enum FanState{
 
 FanState _FanState(Off);
 FanState _NewFanState(Off);
+float FAN_SWITCHABLE_COUNTER = 3; //15 seconds
+float _FanTimerCounter = 3;
 
 void setup() {
   Serial.begin(9600);
@@ -68,33 +72,10 @@ void loop() {
 }
 
 void NormalLoop(){
-  if(_TemperatureSensorCount < TemperatureSensor::Unknown)
-  {
-    Serial.println();
-    Serial.println(F("-------------------"));
-    Serial.println(F("Error : Missing Sensor"));
-
-    display.clearDisplay();
-
-    display.setTextSize(2);   
-    display.setTextColor(SSD1306_WHITE);       
-    display.setCursor(0,0); 
-
-    display.print(F("Error"));
-    display.println();
-    display.print(F("Missing"));
-    display.println();
-    display.print(F("Sensor"));
-    display.display();
-
-    delay(5000);
-
-    return;
-  } 
-
   Serial.println();
   Serial.println(F("-------------------"));
-
+  
+ 
   _InsideTemp = _TemperatureSensors[TemperatureSensor::Inside]->GetTempC();
   _OutsideTemp = _TemperatureSensors[TemperatureSensor::Outside]->GetTempC();
 
@@ -113,18 +94,66 @@ void NormalLoop(){
     _NewFanState = Off;
     //Serial.print(F("Turning OFF fans"));
   }
-} 
+
+  if(_FanState != _NewFanState && _FanTimerCounter >= FAN_SWITCHABLE_COUNTER){
+    if(_NewFanState == On){
+      TurningOnFan();
+    }
+    else{
+      TurningOffFan();
+    }
+    _FanState = _NewFanState;
+  }
+  else if(_FanTimerCounter < FAN_SWITCHABLE_COUNTER){
+    _FanTimerCounter += 1;
+  }
+
+  if(_EditBtn.IsOn()){
+    Serial.println();
+    Serial.println(F("Enable Editing"));
+    _State = Edit;
+    TurningOffFan();
+  }
+
+  delay(5000);
+}
+
+
+void TurningOnFan(){ 
+  Serial.print(F("Turning ON fans"));
+  _FanTimerCounter = 0;
+  _FanState = On;
+  analogWrite(FAN_RELAY_PIN, 200);
+}
+
+void TurningOffFan(){
+  Serial.print(F("Turning OFF fans"));
+  _FanTimerCounter = 0;
+  _FanState = Off;
+  analogWrite(FAN_RELAY_PIN, 0);
+}
 
 void EditLoop(){
 
   if(_UpBtn.IsOn())
     {
-      Serial.print(F("Press Up"));
+      if(_ChangeableTemp < MAX_TEMP)
+      {
+        _ChangeableTemp += 0.5;
+      }
+      Serial.print(F("Changeable Temp : "));
+      Serial.println(_ChangeableTemp);
     }
     if(_DownBtn.IsOn())
     {
-      Serial.print(F("Press Down"));
+      if(_ChangeableTemp > MIN_TEMP)
+      {
+        _ChangeableTemp -= 0.5;
+      }
+      Serial.print(F("Changeable Temp : "));
+      Serial.println(_ChangeableTemp);
     }
+
 
     if(_EditBtn.IsOn())
     {
@@ -133,8 +162,13 @@ void EditLoop(){
       _State = Normal;
     }
 
+    if(_FanTimerCounter < FAN_SWITCHABLE_COUNTER)
+    {
+      _FanTimerCounter += 0.1;
+    }
     delay(500);
 }
+
 
 
 
